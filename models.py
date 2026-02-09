c043cf0ee2af29ab2b1b17423a1ffff934e36ca9
eschol role account <eschol@pub-janeway3-dev.escholarship.org>
2026-02-09 11:00:14 -0800

    Added models for bulk doi refresh info

--- models.py
+++ models.py
@@ -3,7 +3,8 @@ EZID plugin models module
 """
 
 from django.db import models
-
+from journal.models import Journal
+from submission.models import Article
 from repository.models import Repository
 
 class RepoEZIDSettings(models.Model):
@@ -17,3 +18,66 @@ class RepoEZIDSettings(models.Model):
 
     def __str__(self):
         return f"EZID settings: {self.repo}"
+
+
+class TaskStatus(models.IntegerChoices):
+    PENDING = 1, "Pending"
+    IN_PROGRESS = 2, "In Progress"
+    SUCCESS = 3, "Success"
+    FAILURE = 4, "Failure"
+
+class IssueDoiRefreshHistory(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    date_refresh = models.DateTimeField(auto_now_add=True)
+    date_completed = models.DateTimeField(null=True)
+    issue = models.ForeignKey('journal.Issue', on_delete=models.CASCADE)
+
+    result = models.TextField(null=True, blank=True)
+
+    status = models.IntegerField(
+        choices=TaskStatus.choices,
+        default=TaskStatus.PENDING,
+    )
+    def result_text(self):
+        if self.status != TaskStatus.PENDING or self.status != TaskStatus.IN_PROGRESS:
+            total_success = self.ArticleDoiRefreshHistory_set.filter(status=TaskStatus.SUCCESS).count()
+            total = self.ArticleDoiRefreshHistory_set.all().count()
+            return f"Successfully refreshed doi for {total_success} of {total} articles"
+
+        return "Doi refresh in process"
+
+    def __str__(self):
+        success = self.get_status_display() # "successful" if self.success else "failed"
+        s = f"{self.issue} doi refresh {success} on {self.date_refresh}"
+        if self.issue_pub:
+            s += f" with {self.issue_pub.issue}"
+        return s
+
+    class Meta:
+        ordering = ['-date_refresh']
+
+class ArticleDoiRefreshHistory(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    article = models.ForeignKey('submission.Article', on_delete=models.CASCADE)
+    date_refresh = models.DateTimeField(auto_now_add=True)
+    date_completed = models.DateTimeField(null=True)
+    result = models.TextField(null=True, blank=True)
+
+    status = models.IntegerField(
+        choices=TaskStatus.choices,
+        default=TaskStatus.PENDING,
+    )
+    issue_pub = models.ForeignKey('IssueDoiRefreshHistory',
+                                  blank=True,
+                                  null=True,
+                                  on_delete=models.CASCADE)
+
+    def __str__(self):
+        success = self.get_status_display() #"successful" if self.success else "failed"
+        s = f"{self.article} doi refresh {success} on {self.date_refresh}"
+        if self.issue_pub:
+            s += f" with {self.issue_pub.issue}"
+        return s
+
+    class Meta:
+        ordering = ['-date_refresh']
--- views.py
+++ views.py
@@ -2,9 +2,27 @@
 EZID plugin views module (currently placeholder)
 """
 from django.shortcuts import render
-
+from .models import IssueDoiRefreshHistory, ArticleDoiRefreshHistory
+from .plugin_settings import PLUGIN_NAME
+from journal.models import Issue
+from submission.models import Article
+from utils.logger import get_logger
+logger = get_logger(__name__)
 
 def ezid_manager(request):
+    logger.info("In MANAGER")
     template = 'ezid/manager.html'
-    context = {}
+    if request.journal:
+        logger.error("FOUND JOURNAL")
+        issues = Issue.objects.filter(journal=request.journal)
+    else:
+        logger.error("NO JOURNAL IN REQ")
+        issues = Issue.objects.all()
+ 
+    logger.info("The issues are:")
+    logger.info(issues)
+    context = {
+        'plugin_name': PLUGIN_NAME,
+        'issues': issues
+    }
     return render(request, template, context)
