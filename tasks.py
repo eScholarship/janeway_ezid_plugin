448b533c9d7c0fd48187b6339fdceae14b0aac79
eschol role account <eschol@pub-janeway3-dev.escholarship.org>
2026-03-16 13:50:56 -0700

    added check for stage and updated failures in issue to three

--- tasks.py
+++ tasks.py
@@ -14,6 +14,53 @@ from .models import (
 from .logic import update_journal_doi
 
 logger = get_logger(__name__)
+
+def is_refresh_okay(article):
+    """
+    Determines whether it is okay refresh DOI.
+    """
+    if article.stage != "Published":
+        return False, f'Skipping. Expected stage Published, actual {article.stage}'
+    # waiting for more input on date updated check
+    #if article.date_published < article.date_updated:
+    #    return False, f'Skipping. Article updated {article.date_updated} after publishing on {article.date_published}'
+
+    return True, "Okay to proceed"
+
+def refresh_article_doi(article, issueh):
+    """
+    Refreshes one article DOI.
+    """
+    # create article history object
+    history = ArticleDoiRefreshHistory.objects.create(
+        article=article,
+        issue_hist=issueh,
+        date_refresh=timezone.now(),
+    )
+    history.save()
+    success = True # The article skipped are not counted towards failures
+    is_okay, message = is_refresh_okay(article)
+    if is_okay:
+        # skip if the article is not published or updated after publishing
+        logger.info(f"Working on article {article}")
+
+        # do work for each article
+        is_done, is_doi, message = update_journal_doi(article)
+        logger.info(f"result is is_done={is_done} and is_doi={is_doi}")
+
+        success = is_done and is_doi
+
+        history.status = (
+            TaskStatus.SUCCESS if success else TaskStatus.FAILURE
+        )
+    else:
+        history.status = TaskStatus.ABORTED
+
+    history.result = message
+    history.date_completed = timezone.now()
+    history.save()
+    return success
+
 def refresh_issue_doi(issueh_id):
     """
     Task function that Django-Q runs asynchronously to refresh DOIs.
@@ -25,35 +72,15 @@ def refresh_issue_doi(issueh_id):
     except IssueDoiRefreshHistory.DoesNotExist:
         return f"Issuehistory {issueh_id} not found"
 
-    success = False
+    num_failed = 0
 
     # get the list of articles
     for article in issueh.issue.get_sorted_articles():
-        # create article history object
-        history = ArticleDoiRefreshHistory.objects.create(
-            article=article,
-            issue_hist=issueh,
-            date_refresh=timezone.now(),
-        )
-        history.save()
+        success = refresh_article_doi(article, issueh)
+        num_failed += int(not success)
 
-        logger.info(f"Working on article {article}")
-
-        # do work for each article
-        is_done, is_doi, message = update_journal_doi(article)
-        logger.info(f"result is is_done={is_done} and is_doi={is_doi}")
-
-        success = is_done and is_doi
-
-        history.result = message
-        history.status = (
-            TaskStatus.SUCCESS if success else TaskStatus.FAILURE
-        )
-        history.date_completed = timezone.now()
-        history.save()
-
-        # if failed then stop
-        if not success:
+        # if failed three times then stop
+        if num_failed > 3:
             break
 
     issueh.status = TaskStatus.SUCCESS if success else TaskStatus.FAILURE
